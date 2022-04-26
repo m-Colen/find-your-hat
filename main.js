c4c0b111c00d54b38406e6b413724662012cc51f
m-colen <mzcolen@gmail.com>
2022-04-25 20:53:43 -0500

    Added random character generator

--- main.js
+++ main.js
@@ -63,6 +63,59 @@ class Field {
   print() {
     this.fieldArrays.forEach(element => console.log(element.join(' ')));
   }
+  
+  // Static method to generate field
+  static generateField(width, height, holePercentage) {
+
+    const newArray = [];  
+    let subArray = [];  
+
+    // Generating random characters 
+    const numOfCharacters = width * height; 
+    const numOfHoles = numOfCharacters * holePercentage / 100;
+    const numOfFieldChars = numOfCharacters - numOfHoles;  
+    const randArray = [];
+
+    for (let i = 0; i < numOfHoles; i++) {    
+      randArray.push(hole);
+    }
+    for (let i = 0; i < numOfFieldChars; i ++) {
+      randArray.push(fieldCharacter);
+    }
+    
+    // Pushing random array items to subArray
+    while (subArray.length < width) {
+      const randomIndex = Math.floor(Math.random() * randArray.length);
+      const splice = randArray.splice(randomIndex, 1);
+      subArray.push(splice);
+      if (subArray.length === width) {
+        newArray.push(subArray);
+        subArray = [];
+      }
+      if (newArray.length === height) {
+        break;
+      }
+    }
+    
+    // Setting starting point
+    newArray[0][0] = [pathCharacter];
+    
+    // Random hat placement
+    const generateRandomHeight = () => {
+      const randomHeight = Math.floor(Math.random() * height);
+      return randomHeight;
+    };
+    
+    const generateRandomWidth = () => {
+      const randomWidth = Math.floor(Math.random() * width);
+      return randomWidth;
+    }
+    
+    newArray[generateRandomHeight()][generateRandomWidth()] === [pathCharacter] ? newArray[generateRandomHeight()][generateRandomWidth() + 1] = [hat] : newArray[generateRandomHeight()][generateRandomWidth()] = [hat];
+    
+    return newArray;
+      
+  }
 
   // Method to play game
   play() {
@@ -71,7 +124,7 @@ class Field {
     while (this.gameOver === false) {
       this.print(); // Prints field
       let direction = prompt('Which way (u, d, l, r)?');
-      direction = direction.toLowerCase(); // So switch statement works properly
+      direction = direction.toLowerCase(); // To match user input to switch statement
       try {
         switch(direction) {
           case "d":
@@ -89,35 +142,37 @@ class Field {
         }
 
         this.currentPosition = this.fieldArrays[this.indexY][this.indexX];
+        const currentItem = this.currentPosition.shift();
 
         // Updates characters based on movement
       
-        switch(this.currentPosition) {
+        switch(currentItem) {
           case fieldCharacter:
-            this.fieldArrays[this.indexY][this.indexX] = pathCharacter;
+            this.fieldArrays[this.indexY][this.indexX] = [pathCharacter];
             break;
           case hole:
             console.log('Sorry, you fell in a hole!');
             this.fieldArrays[this.indexY][this.indexX] = 'X';
-            this.print();
             this.gameOver = true;
+            this.print();
             break;
           case hat:
             console.log('Congrats, you won!!!');
             this.fieldArrays[this.indexY][this.indexX] = 'X';
-            this.print();
             this.gameOver = true;
+            this.print();
             break;
           case pathCharacter:
+            console.log(`You've already been here`);
             this.fieldArrays[this.indexY][this.indexX] = '✱';
             break;
           default:
+            console.log(currentItem);
             console.log('Sorry, you went out of bounds');
-            this.gameOver = true;
             break;
         } 
       } catch (e) {
-        console.log('Sorry you went out of bounds');
+        console.log('Sorry, you went out of bounds');
         this.gameOver = true;
         }
     }
@@ -127,11 +182,7 @@ class Field {
 
 
 // Class instance to test
-const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
-]);
+const myField = new Field(Field.generateField(10, 10, 25));
 
 // Starts game
 myField.play();
